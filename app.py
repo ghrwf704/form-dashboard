7da2c7e5607be76013d66085b0a9e0fce7b408d6
ghrwf704@yahoo.co.jp <ghrwf704@yahoo.co.jp>
2025-05-27 12:37:01 +0900

    Add HEAD support to / for health check compatibility

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
-from pymongo import MongoClient
+import os
+import time
+import pymongo
 import certifi
+from flask import Flask, render_template
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+from form_detector import detect_forms
 
-app = Flask(__name__)
+URLS_FILE = "urls_raw.txt"
 
-uri = "mongodb+srv://ykeikeikie:qMUerl78WgsEEOWA@cluster0.helfbov.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
-
+# MongoDBÇ…ê⁄ë±ÅiMongoDB Atlas ÇÃURI ÇÇ±Ç±Ç…ê›íËÅj
+MONGO_URI = "mongodb+srv://ykeikeikie:qMUerl78WgsEEOWA@cluster0.helfbov.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
 db = client["form_database"]
 collection = db["forms"]
 
-@app.route("/")
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "HEAD"])
 def index():
     forms = list(collection.find().sort("_id", -1))
     return render_template("index.html", forms=forms)
